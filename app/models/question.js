be47f9c254df3f5b7e03b56039649039d4b15451
Derek Edwards <derekbedwards@gmail.com>
2016-06-29 14:29:20 -0700

    Fix question model

--- app/models/question.js
+++ app/models/question.js
@@ -17,17 +17,17 @@ export default Model.extend({
 
   isText: function() {
     return (this.get('type') === '2');
-  }.property('this.type'),
+  }.property('type'),
 
   notText: function() {
     return (this.get('type') !== '2');
-  }.property('this.type'),
+  }.property('type'),
 
   isMultiChoice: function() {
     return (this.get('type') === '1');
-  }.property('this.type'),
+  }.property('type'),
 
   isSingleChoice: function() {
     return (this.get('type') === '0');
-  }.property('this.type')
+  }.property('type')
 });
